eea9cd7654595a7634d4a0cadca3784b271d8ef0
Nicolai Mainiero <nicolai@mainiero.de>
2022-06-16 14:13:45 +0200

    Initial commit

---
+++ rust-adder/Main.java
@@ -0,0 +1,7 @@
+import static de.sidion.rust.lib_h.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("23 + 42 = " + rust_add_numbers(23,42));
+    }
+}
